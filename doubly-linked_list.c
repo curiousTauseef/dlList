5cb28a27bbb8af58a8af93d0f8fbc1fe14eb46bf
Lokalbruker <lokal@hpebian.norbit.no>
2013-03-22 12:12:07 +0100

    Coding style touch-up + finishing sorting and find functions.

--- doubly-linked_list.c
+++ doubly-linked_list.c
@@ -1,8 +1,11 @@
 #include "doubly-linked_list.h"
 #include <stdlib.h>
 
-void dlList_init(struct dlList *list, void (*destroyFunction)(void *data), int (*compareFunction)(const void *data1, const void *data2)){
-	if(list == NULL)
+void dlList_init( struct dlList *list,
+      void ( *destroyFunction )( void *data ),
+      int ( *compareFunction )( const void *data1, const void *data2 ) )
+{
+	if ( !list )
 		return;
 
 	list->size = 0;
@@ -12,40 +15,45 @@ void dlList_init(struct dlList *list, void (*destroyFunction)(void *data), int (
 	list->destroy = destroyFunction;
 }
 
-void dlList_destroy(struct dlList *list){
-	if(list == NULL)
+void dlList_destroy( struct dlList *list )
+{
+	if ( !list )
 		return;
 
-	while(list->size > 0)
-		dlList_remove(list, list->tail, NULL);
+	while ( list->size > 0 )
+		dlList_remove( list, list->tail, NULL );
 
 	list->head = NULL;
 	list->tail = NULL;
 	list->size = 0;
 }
 
-int dlList_insertBefore(struct dlList *list, struct dlList_node *beforeNode, void *data){
-	if(list == NULL || (list->size > 0 && beforeNode == NULL))
-		return -1;
+int dlList_insertBefore( struct dlList *list, struct dlList_node *beforeNode,
+      void *data )
+{
+	if ( !list || ( !beforeNode && list->size > 0 ) )
+		return DLLIST_ERR_errArg;
 
-	struct dlList_node *newNode = malloc(sizeof(struct dlList_node));
-	if(newNode == NULL)
-		return -1;
+	struct dlList_node *newNode = malloc( sizeof( struct dlList_node ) );
+	if ( !newNode )
+		return DLLIST_ERR_malloc;
 	
 	newNode->data = data;
 
-	if(list->size == 0){
+	if ( list->size == 0 )
+   {
 		newNode->next = NULL;
 		newNode->prev = NULL;
 		list->head = newNode;
 		list->tail = newNode;
 	}
 
-	else{
+	else
+   {
 		newNode->next = beforeNode;
 		newNode->prev = beforeNode->prev;
 
-		if(beforeNode->prev == NULL)
+		if ( beforeNode->prev == NULL )
 			list->head = newNode;
 		else
 			beforeNode->prev->next = newNode;
@@ -55,31 +63,35 @@ int dlList_insertBefore(struct dlList *list, struct dlList_node *beforeNode, voi
 
 	++list->size;
 
-	return 0;
+	return DLLIST_OK;
 }
 
-int dlList_insertAfter(struct dlList *list, struct dlList_node *afterNode, void *data){
-	if(list == NULL || (afterNode == NULL && list->size > 0))
-		return -1;
+int dlList_insertAfter( struct dlList *list, struct dlList_node *afterNode,
+      void *data )
+{
+	if ( !list || ( !afterNode && list->size > 0 ) )
+      return DLLIST_ERR_errArg;
 
-	struct dlList_node *newNode = malloc(sizeof(struct dlList_node));
-	if(newNode == NULL)
-		return -1;
+	struct dlList_node *newNode = malloc( sizeof( struct dlList_node ) );
+   if ( !newNode )
+      return DLLIST_ERR_malloc;
 
 	newNode->data = data;
 
-	if(list->size == 0){
+	if ( list->size == 0 )
+   {
 		newNode->next = NULL;
 		newNode->prev = NULL;
 		list->head = newNode;
 		list->tail = newNode;
 	}
 
-	else{
+	else
+   {
 		newNode->next = afterNode->next;
 		newNode->prev = afterNode;
 
-		if(afterNode->next == NULL)
+		if ( !afterNode->next )
 			list->tail = newNode;
 		else
 			afterNode->next->prev = newNode;
@@ -89,65 +101,175 @@ int dlList_insertAfter(struct dlList *list, struct dlList_node *afterNode, void 
 
 	++list->size;
 
-	return 0;
+   return DLLIST_OK;
 }
 
-int dlList_append(struct dlList *list, void *data){
-	if(list == NULL)
-		return -1;
+int dlList_append( struct dlList *list, void *data )
+{
+   if ( !list )
+      return DLLIST_ERR_errArg;
 
-	return dlList_insertAfter(list, list->tail, data);
+	return dlList_insertAfter( list, list->tail, data );
 }
 
-int dlList_remove(struct dlList *list, struct dlList_node *node, void **data){
-	if(list == NULL || list->size == 0 || node == NULL)
-		return -1;
+int dlList_remove( struct dlList *list, struct dlList_node *node,
+      void **data )
+{
+	if ( !list || list->size == 0 || !node )
+      return DLLIST_ERR_errArg;
 
-	if(data != NULL)
+	if ( data )
 		*data = node->data;
-	else if(list->destroy != NULL)
+
+	else if ( list->destroy )
 		list->destroy(node->data);
 
-	if(node == list->head){
+	if ( node == list->head )
+   {
 		list->head = node->next;
 
-		if(list->head == NULL)
+		if ( !list->head )
 			list->tail = NULL;
 		else
 			node->next->prev = NULL;
 	}
 	
-	else{
+	else
+   {
 		node->prev->next = node->next;
 
-		if(node->next == NULL)
+		if ( !node->next )
 			list->tail = node->prev;
 		else
 			node->next->prev = node->prev;
 	}
 
-	free(node);
+	free( node );
 
 	--list->size;
 
-	return 0;
+   return DLLIST_OK;
 }
 
-//struct dlList_node *dlList_find(struct dlList *list, const void *key){
-//	if(list == NULL || list->compare == NULL || key == NULL)
-//		return NULL;
-//
-//	struct dlList_node *nodePointer = list->head;
-//
-//	while(nodePointer != NULL || list->compare(&key, &nodePointer->data) != 0)
-//		nodePointer = nodePointer->next;
-//
-//	return nodePointer;
-//}
+struct dlList_node *dlList_find( const struct dlList *list, const void *key )
+{
+	if ( !list || !list->compare || !key )
+		return NULL;
 
-unsigned int dlList_getSize(struct dlList *list){
-	if(list == NULL)
+	struct dlList_node *nodePointer = list->head;
+
+	while ( nodePointer && list->compare( &key, &nodePointer->data) != 0)
+		nodePointer = nodePointer->next;
+
+	return nodePointer;
+}
+
+//FIXME:
+//struct dlList *dlList_findAll( const struct dlList *list, const void *key );
+
+void dlList_sort( struct dlList *list )
+{
+   if ( !list )
+      return;
+
+   if ( list->size <= 1 )
+      return;
+
+   /* The following algorithm is based on http://stackoverflow.com/a/3032462
+    * */
+
+   int numMerges;
+   size_t leftSize, rightSize, listSize = 1;
+   struct dlList_node *tail, *left, *right, *next;
+
+   do {
+      numMerges = 0;
+      left = list->head;
+      tail = list->head = NULL;
+
+      while ( left )
+      {
+         ++numMerges;
+         right = left;
+         leftSize = 0;
+         rightSize = listSize;
+
+         while ( right && leftSize < listSize )
+         {
+            ++leftSize;
+            right = right->next;
+         }
+
+         while ( leftSize > 0 || ( right && rightSize > 0 ) )
+         {
+            if ( !leftSize )
+            {
+               next = right;
+               right = right->next;
+               --rightSize;
+            }
+
+            else if ( !right || !rightSize )
+            {
+               next = left;
+               left = left->next;
+               --leftSize;
+            }
+
+            else if ( list->compare( &left->data, &right->data ) < 0 )
+            {
+               next = left;
+               left = left->next;
+               --leftSize;
+            }
+
+            else
+            {
+               next = right;
+               right = right->next;
+               --rightSize;
+            }
+            
+            if ( tail )
+               tail->next = next;
+            else
+               list->head = next;
+
+            next->prev = tail;
+            tail = next;
+         }
+
+         left = right;
+      }
+
+      tail->next = NULL;
+      listSize <<= 1;
+
+   } while ( numMerges > 1 );
+
+   list->tail = tail;
+}
+
+size_t dlList_size( const struct dlList *list )
+{
+	if ( !list )
 		return 0;
 
 	return list->size;
+}
+
+struct dlList_node *dlList_first( const struct dlList *list )
+{
+   if ( !list )
+      return NULL;
+
+   return list->head;
+}
+
+struct dlList_node *dlList_last( const struct dlList *list )
+{
+   if ( !list )
+      return NULL;
+
+   return list->tail;
 }
--- doubly-linked_list.h
+++ doubly-linked_list.h
@@ -1,40 +1,60 @@
 #ifndef DOUBLYLINKED_LIST
 #define DOUBLYLINKED_LIST
 
-struct dlList_node{
+#include <stddef.h>
+
+struct dlList_node
+{
 	void *data;
 	struct dlList_node *prev;
 	struct dlList_node *next;
 };
 
+typedef struct dlList dlList;
 
 struct dlList{
-	unsigned int size;
+	size_t size;
 	struct dlList_node *head;
 	struct dlList_node *tail;
 
-	int (*compare)(const void *data1, const void *data2);
-	void (*destroy)(void *data);
+	int ( *compare )( const void *data1, const void *data2 );
+	void ( *destroy )( void *data );
 };
 
-void dlList_init(struct dlList *list, void (*destroyFunction)(void *data), int (*compareFunction)(const void *data1, const void *data2));
+enum dlList_returnValues
+{
+   DLLIST_OK,
+   DLLIST_ERR_errArg,
+   DLLIST_ERR_malloc,
+};
 
-void dlList_destroy(struct dlList *list);
+void dlList_init( struct dlList *list,
+      void ( *destroyFunction )( void *data ),
+      int ( *compareFunction )( const void *data1, const void *data2) );
 
-int dlList_insertBefore(struct dlList *list, struct dlList_node *beforeNode, void *data);
+void dlList_destroy( struct dlList *list );
 
-int dlList_insertAfter(struct dlList *list, struct dlList_node *afterNode, void *data);
+int dlList_insertBefore( struct dlList *list, struct dlList_node *beforeNode,
+      void *data );
 
-int dlList_append(struct dlList *list, void *data);
+int dlList_insertAfter( struct dlList *list, struct dlList_node *afterNode,
+      void *data );
 
-int dlList_remove(struct dlList *list, struct dlList_node *node, void **data);
+int dlList_append( struct dlList *list, void *data );
 
-struct dlList_node *dlList_find(struct dlList *list, const void *key);
+int dlList_remove( struct dlList *list, struct dlList_node *node,
+      void **data );
 
-struct dlList *dlList_findAll(struct dlList *list, const void *key);
+struct dlList_node *dlList_find( const struct dlList *list, const void *key );
 
-void dlList_sort(struct dlList *list);
+struct dlList *dlList_findAll( const struct dlList *list, const void *key );
 
-unsigned int dlList_getSize(struct dlList *list);
+void dlList_sort( struct dlList *list );
+
+size_t dlList_size( const struct dlList *list );
+
+struct dlList_node *dlList_first( const struct dlList *list );
+
+struct dlList_node *dlList_last( const struct dlList *list );
 
 #endif //DOUBLYLINKED_LIST
